1ec1a4590bf74bbc6c14c78914200a33faf5fb74
Clément Nancelle <clement.nancelle@epitech.eu>
2018-01-27 19:04:38 +0100

    wall and soundmanager

---
+++ Global Game Jam/Assets/Wall.cs
@@ -0,0 +1,25 @@
+﻿    using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public AudioClip ChopSound1;
+    public AudioClip ChopSound2;
+    public Sprite DmgSprite;
+    public int Hp = 4;
+
+    private SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        SoundManager.Instance.RandomizeSfx(ChopSound1, ChopSound2);
+        _spriteRenderer.sprite = DmgSprite;
+        Hp -= loss;
+        if (Hp <= 0)
+            gameObject.SetActive(false);
+    }
+}
