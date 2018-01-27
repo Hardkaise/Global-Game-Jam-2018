5a28a44879b0003a629bfe58185d3af03e609ac8
VianneyDoleans <vianney.doleans@epitech.eu>
2018-01-28 00:55:02 +0100

    werewolf move debug

--- Global Game Jam/Assets/player.cs
+++ Global Game Jam/Assets/player.cs
@@ -10,6 +10,8 @@ public class player : MonoBehaviour {
 	void Start () {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        anim.SetTrigger("wait");
+        anim.SetBool("check", true);
     }
 
 	void FixedUpdate () {
@@ -17,17 +19,34 @@ public class player : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        if (moveVertical > 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
+        if (Input.GetKey(KeyCode.LeftArrow) && anim.GetBool("check") == true)
         {
-           // Debug.Log("enter");
+            anim.SetBool("check", false);
+            anim.SetTrigger("left");
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && anim.GetBool("check") == true)
+        {
+            anim.SetBool("check", false);
+            anim.SetTrigger("down");
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && anim.GetBool("check") == true)
+        {
+            anim.SetBool("check", false);
             anim.SetTrigger("up");
         }
-        else if (moveVertical < 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
-            anim.SetTrigger("down");
-        else if (moveHorizontal > 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
+        if (Input.GetKey(KeyCode.RightArrow) && anim.GetBool("check") == true)
+        {
+            anim.SetBool("check", false);
             anim.SetTrigger("right");
-        else if (moveHorizontal < 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
-            anim.SetTrigger("left");
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("left") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.LeftArrow))
+            anim.SetBool("check", true);
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("down") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.DownArrow))
+            anim.SetBool("check", true);
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("right") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.RightArrow))
+            anim.SetBool("check", true);
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("up") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.UpArrow))
+            anim.SetBool("check", true);
         movement.x *= (float)0.1;
         movement.y *= (float)0.1;
         var moveX = body.transform.position.x;
