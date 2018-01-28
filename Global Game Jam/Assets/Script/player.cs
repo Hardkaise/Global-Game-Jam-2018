73cfdd20ac0241864e89e60f01d1f42022b8c9c6
Clément Nancelle <clement.nancelle@epitech.eu>
2018-01-28 01:50:35 +0100

    Auto stash before merge of "master" and "origin/master"

--- Global Game Jam/Assets/Script/player.cs
+++ Global Game Jam/Assets/Script/player.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
     public Rigidbody2D Body;
@@ -12,27 +12,44 @@ public class player : MonoBehaviour
     {
         Body = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _anim.SetBool("check", true);
     }
 
     void FixedUpdate()
     {
-
-        var moveHorizontal = Input.GetAxis("Horizontal");
-        var moveVertical = Input.GetAxis("Vertical");
-        var movement = new Vector2(moveHorizontal, moveVertical);
-        if (moveVertical > 0 && _anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if (Input.GetKey(KeyCode.LeftArrow) && _anim.GetBool("check") == true)
         {
-            // Debug.Log("enter");
+            _anim.SetBool("check", false);
+            _anim.SetTrigger("left");
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && _anim.GetBool("check") == true)
+        {
+            _anim.SetBool("check", false);
+            _anim.SetTrigger("down");
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && _anim.GetBool("check") == true)
+        {
+            _anim.SetBool("check", false);
             _anim.SetTrigger("up");
         }
-        else if (moveVertical < 0 && _anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
-            _anim.SetTrigger("down");
-        else if (moveHorizontal > 0 && _anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
+        if (Input.GetKey(KeyCode.RightArrow) && _anim.GetBool("check") == true)
+        {
+            _anim.SetBool("check", false);
             _anim.SetTrigger("right");
-        else if (moveHorizontal < 0 && _anim.GetCurrentAnimatorStateInfo(0).IsName("wait"))
-            _anim.SetTrigger("left");
-        movement.x *= (float) 0.1;
-        movement.y *= (float) 0.1;
+        }
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("left") && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.LeftArrow))
+            _anim.SetBool("check", true);
+        else if (_anim.GetCurrentAnimatorStateInfo(0).IsName("down") && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.DownArrow))
+            _anim.SetBool("check", true);
+        else if (_anim.GetCurrentAnimatorStateInfo(0).IsName("right") && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.RightArrow))
+            _anim.SetBool("check", true);
+        else if (_anim.GetCurrentAnimatorStateInfo(0).IsName("up") && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.25f && !Input.GetKey(KeyCode.UpArrow))
+            _anim.SetBool("check", true);
+        movement.x *= (float)0.1;
+        movement.y *= (float)0.1;
         var moveX = Body.transform.position.x;
         var moveY = Body.transform.position.y;
         Body.transform.position = new Vector3(moveX + movement.x, moveY + movement.y, 0);
